d8c5959e492c5bd4fb4dc8bdc00c71da57dd1dbc
elliotwoods <elliot.woods@gmail.com>
2011-06-19 22:25:48 +0900

    removed rogue Data dirctior y

--- Data/pfitBasis.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  pfitBasis.cpp
-//  ofxPolyfitExample
-//
-//  Created by Elliot Woods on 19/06/2011.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
-#include "pfitBasis.h"
--- Data/pfitBasis.h
+++
@@ -1,7 +0,0 @@
-//
-//  pfitBasis.h
-//  ofxPolyfitExample
-//
-//  Created by Elliot Woods on 19/06/2011.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
